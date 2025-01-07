383bb279a78ad8148ff760e26b4c9fea46e259fe
KING-HANSA <hansadewmina2008@gmail.com>
2025-01-07 21:59:33 +0530

    Update config.js

--- config.js
+++ config.js
@@ -7,6 +7,6 @@ function convertToBool(text, fault = 'true') {
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "iJxXUbIB#2YdZCVMoLiF-kiwHprgQfs09Qsc4kuHLdPmR1MIv3H8",
 MONGODB: process.env.MONGODB || "mongodb+srv://athulakumara604:qBwvqo6IM64eT1SL@cluster0.wr7rx.mongodb.net/",
-GITHUB_USERNAME: process.env.GITHUB_USERNAME === undefined ? 'KING-HANSA': process.env.GITHUB_USERNAME,
+GITHUB_USERNAME: process.env.GITHUB_USERNAME === undefined ? 'Asithabzkejx': process.env.GITHUB_USERNAME,
 GITHUB_AUTH_TOKEN: process.env.GITHUB_AUTH_TOKEN === undefined ? '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': process.env.GITHUB_AUTH_TOKEN
 };
