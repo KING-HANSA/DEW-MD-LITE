2bd9178a7f37eb21ee8824e7770278eb0cf6aff9
KING-HANSA <hansadewmina2008@gmail.com>
2025-01-07 20:20:48 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,13 +5,13 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "XDoDGSbD#qfoi5Q0UDdM7Eh9m38cNQvHRFmksQELxJ8FdlirUWGk",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://i.ibb.co/dfkL19B/20241019-101138.jpg",
-ALIVE_MSG: process.env.ALIVE_MSG || "HELLOW THEIR, KING-HANSA IS ALIVE...\n\nOWNER: HANSA DEWMINA\n\nTHANKS FOR USING KING-HANSA-LITE\n\n> *© ᴋɪɴɢ ʜᴀɴꜱᴀ ʟɪᴛᴇ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ*/n/n*> ɢɪᴛʜᴜʙ :* github.com/KING-HANSA/KING-HANSA-LITE/",
+SESSION_ID: process.env.SESSION_ID || "iJxXUbIB#2YdZCVMoLiF-kiwHprgQfs09Qsc4kuHLdPmR1MIv3H8",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.imgur.com/JL9z4b0.jpeg",
+ALIVE_MSG: process.env.ALIVE_MSG || "HELLOW THEIR, DEW-MD IS ALIVE...\n\nOWNER: HANSA DEWMINA\n\nTHANKS FOR USING DEW-MD-LITE\n\n> *© ᴅᴇᴡ-ᴍᴅ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ*",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
-BOT_NUMBER: process.env.BOT_NUMBER || "94701515609",
-OWNER_REACT: process.env.OWNER_REACT || "🍁",
+BOT_NUMBER: process.env.BOT_NUMBER || "94778721239",
+OWNER_REACT: process.env.OWNER_REACT || "🪀",
 OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39",
 READ_CMD: process.env.READ_CMD || "true",
 AUTO_VOICE:"true"
