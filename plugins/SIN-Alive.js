50ae5042f3192918ad6baa2951b5e6293c0e8507
KING-HANSA <hansadewmina2008@gmail.com>
2025-01-08 20:48:08 +0530

    Create SIN-Alive.js

---
+++ plugins/SIN-Alive.js
@@ -0,0 +1,60 @@
+
+/**
+██████╗ ███████╗██╗    ██╗    ███╗   ███╗██████╗     ██╗     ██╗████████╗███████╗
+██╔══██╗██╔════╝██║    ██║    ████╗ ████║██╔══██╗    ██║     ██║╚══██╔══╝██╔════╝
+██║  ██║█████╗  ██║ █╗ ██║    ██╔████╔██║██║  ██║    ██║     ██║   ██║   █████╗  
+██║  ██║██╔══╝  ██║███╗██║    ██║╚██╔╝██║██║  ██║    ██║     ██║   ██║   ██╔══╝  
+██████╔╝███████╗╚███╔███╔╝    ██║ ╚═╝ ██║██████╔╝    ███████╗██║   ██║   ███████╗
+╚═════╝ ╚══════╝ ╚══╝╚══╝     ╚═╝     ╚═╝╚═════╝     ╚══════╝╚═╝   ╚═╝   ╚══════╝                                                                            
+         
+* Project name - DEW-MD-LITE 
+* Author - Hansa Dewmina
+* Team - Dewminacoders 
+* Version - vX
+
+World best and powerfull whatsapp user bot in Sri lanka*/
+
+const config = require('../config')
+const {cmd , commands} = require('../command')
+const os = require("os")
+const {runtime} = require('../lib/functions')
+
+cmd({
+    pattern: "ඉන්නවද",
+    desc: "To Check the bot online or no.",
+    react: "💌",
+    category: "main",
+    filename: __filename
+},
+async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
+try{
+
+const voice = {
+    alive: 'media/media_alive.mp3'
+}
+
+let aliveMessage = ` 
+👋*හායි ලස්සන ලමයෝ* ${pushname}
+
+╭────《 *මම ඉන්නවා* 》
+│ ╭───────────◆
+│ │ Owner Name: Hansa
+│ │ Owner Number:${config.BOT_NUMBER}
+│ │ Uptime:${runtime(process.uptime())}
+│ │ Host Name:${os.hostname()}
+│ │ RAM Usage:${(process.memoryUsage().heapUsed / 1024 / 1024).toFixed(2)}MB / ${Math.round(require('os').totalmem / 1024 / 1024)}MB
+│ ╰───────────◆
+╰───────────⊷
+
+*DEW - MD LITE*
+> Dew-Coders™`
+    
+await conn.sendMessage(from, { audio: { url: voice.alive }, mimetype: 'audio/mp4', ptt: true }, { quoted: mek })
+
+return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption:aliveMessage},{quoted: mek})
+
+}catch(e){
+console.log(e)
+reply(`ALIVE MAGAGE WORKING ON WHATSAPP ✔`)
+}
+})
