1a25e6785e9e98bcbdc216f1fb6b9939579cea4e
KING-HANSA <hansadewmina2008@gmail.com>
2025-01-08 14:51:02 +0530

    Update _alive.js

--- plugins/_alive.js
+++ plugins/_alive.js
@@ -1,48 +1,25 @@
-const config = require('../config')
+const {readEnv} = require('../lib/database')
 const {cmd , commands} = require('../command')
-const os = require("os")
-const {runtime} = require('../lib/functions')
 
 cmd({
     pattern: "alive",
-    desc: "To Check the bot online or no.",
-    react: "💌",
+    desc: "Check bot online or no.",
     category: "main",
+    react: "👋",
     filename: __filename
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
 
-const voice = {
+    const voice = {
     alive: 'media/media_alive.mp3'
 }
 
-let aliveMessage = ` 
-🍁»»» 𝐊𝐈𝐍𝐆 𝐇𝐀𝐍𝐒𝐀-𝐋𝐈𝐓𝐄 »»»🍁
-
-👋𝗛𝗶⚡ ${pushname}!
-
- ╭─「 *𝐈 𝐚𝐦 𝐀𝐥𝐢𝐯𝐞 𝐍𝐨𝐰* 」
-│◈ *яυηтιмє* - ${runtime(process.uptime())}
-│◈ *σωηєя ηαмє* - 𝐇𝐚𝐧𝐬𝐚 𝐃𝐞𝐰𝐦𝐢𝐧𝐚
-│◈ *σωηєя ηυмвєя* - ${config.BOT_NUMBER}
-╰──────────●●►
-
-*𝒮𝓉𝒶𝓎 𝒸𝑜𝓃𝓃𝑒𝒸𝓉𝑒𝒹 𝒶𝓃𝒹 𝑒𝓃𝒿𝑜𝓎 𝓉𝒽𝑒 𝓈𝑒𝓇𝓋𝒾𝒸𝑒𝓈!* 
-╭──────────●●►
-│© 𝐊𝐈𝐍𝐆 𝐇𝐀𝐍𝐒𝐀-𝐋𝐈𝐓𝐄 - 𝐌𝐃* 
-│🍁 *GitHub*: *github.com/KING-HANSA/KING-HANSA-LITE* 
-╰───────────●●►`
-    
+const config = await readEnv();
 await conn.sendMessage(from, { audio: { url: voice.alive }, mimetype: 'audio/mp4', ptt: true }, { quoted: mek })
-
-return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption:aliveMessage},{quoted: mek})
-
+return await conn.sendMessage(from,{image: {url: config.ALIVE_IMG},caption: config.ALIVE_MSG},{quoted: mek})
 }catch(e){
 console.log(e)
-reply(`ALIVE MAGAGE WORKING ON WHATSAPP ✔`)
+reply(`${e}`)
 }
 })
-
-
-
