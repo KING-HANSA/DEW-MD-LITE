cf5ae5413269b37c3de2e29a620671cbea3b22e6
KING-HANSA <hansadewmina2008@gmail.com>
2025-01-08 14:58:22 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,11 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "iJxXUbIB#2YdZCVMoLiF-kiwHprgQfs09Qsc4kuHLdPmR1MIv3H8",
-MONGODB: process.env.MONGODB || "mongodb+srv://kulathungaasitha319:yjHB0DvFfStNfwPS@cluster0.3oijd.mongodb.net/",
-BOT_NUMBER: process.env.BOT_NUMBER || "94778721239",
-OWNER_REACT: process.env.OWNER_REACT || "🪀",
-OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39",
-READ_CMD: process.env.READ_CMD || "true",
-AUTO_VOICE:"true"
+SESSION_ID: process.env.SESSION_ID || "7QBykTCR#dtibamD9C6OF4tZuca31GlFGYQuQdsybJ-Mhc56vXIo",
+MONGODB: process.env.MONGODB || "mongodb+srv://username:password@cluster.mongodb.net/?retryWrites=true&w=majority",
 };
