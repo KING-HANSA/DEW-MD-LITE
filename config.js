2c8478cc5f67a2e31b403ab1be44b074d85d2b59
KING-HANSA <hansadewmina2008@gmail.com>
2025-01-19 17:34:31 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,15 +5,19 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "iJxXUbIB#2YdZCVMoLiF-kiwHprgQfs09Qsc4kuHLdPmR1MIv3H8",
-ALIVE_IMG: process.env.ALIVE_IMG || "https://i.imgur.com/JL9z4b0.jpeg",
-ALIVE_MSG: process.env.ALIVE_MSG || "HELLOW THEIR, DEW-MD IS ALIVE...\n\nOWNER: HANSA DEWMINA\n\nTHANKS FOR USING DEW-MD-LITE\n\n> *© ᴅᴇᴡ-ᴍᴅ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ*",
+SESSION_ID: process.env.SESSION_ID || "enter your session",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "public",
-BOT_NUMBER: process.env.BOT_NUMBER || "94778721239",
-OWNER_REACT: process.env.OWNER_REACT || "🪀",
-OMDB_API_KEY: process.env.OMDB_API_KEY || "76cb7f39",
-READ_CMD: process.env.READ_CMD || "true",
-AUTO_VOICE:"true"
+AUTO_VOICE: process.env.AUTO_VOICE || "true",
+AUTO_STICKER: process.env.AUTO_STICKER || "true",
+AUTO_REPLY: process.env.AUTO_REPLY || "true",
+ALIVE_IMG: process.env.ALIVE_IMG || "https://i.imgur.com/YXp71EV.jpeg",
+ALIVE_MSG: process.env.ALIVE_MSG || "HELLOW THEIR, DEW-MD IS ALIVE...\n\nOWNER: HANSA DEWMINA\n\nTHANKS FOR USING DEW-MD-LITE\n\n> *© ᴅᴇᴡ-ᴍᴅ ᴡʜᴀᴛꜱᴀᴘᴘ ʙᴏᴛ*",
+ANTI_LINK: process.env.ANTI_LINK || "true",
+ANTI_BAD: process.env.ANTI_BAD || "true",
+PREFIX: process.env.PREFIX || ".",
+FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
+AUTO_REACT: process.env.AUTO_REACT || "true",
+OWNER_REACT: process.env.OWNER_REACT || "true",
+BOT_NAME: process.env.BOT_NAME || "DEW-MD-LITE",
 };
-
