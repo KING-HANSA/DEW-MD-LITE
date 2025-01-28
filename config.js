03db814d955b15db8018bae2dd5561ee653715dd
KING-HANSA <hansadewmina2008@gmail.com>
2025-01-28 12:33:53 +0530

    Update config.js

--- config.js
+++ config.js
@@ -6,9 +6,6 @@ function convertToBool(text, fault = 'true') {
 }
 module.exports = {
 SESSION_ID: process.env.SESSION_ID || "XNR11YIQ#FZuTHzVR8JYnbCp17INiQIVG5VZlNHS5m9t-RB-YnAc",
-AUTO_VOICE: process.env.AUTO_VOICE || "true",
-AUTO_STICKER: process.env.AUTO_STICKER || "true",
-AUTO_REPLY: process.env.AUTO_REPLY || "true",
 ANTI_LINK: process.env.ANTI_LINK || "true",
 ANTI_BAD: process.env.ANTI_BAD || "true",
 FAKE_RECORDING: process.env.FAKE_RECORDING || "true",
