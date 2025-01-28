f4359b2b82a6e9a19b7b93c33ffeb2f2e50f8d34
KING-HANSA <hansadewmina2008@gmail.com>
2025-01-28 11:39:23 +0530

    Update weather.js

--- plugins/weather.js
+++ plugins/weather.js
@@ -28,7 +28,7 @@ async (conn, mek, m, { from, q, reply }) => {
 💨 *Wind Speed*: ${data.wind.speed} m/s
 🔽 *Pressure*: ${data.main.pressure} hPa
 
-*POWERED BY SOBIA MD*
+*POWERED BY DEW-MD*
 `;
         return reply(weather);
     } catch (e) {
