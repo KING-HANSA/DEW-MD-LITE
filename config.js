b7d83afbf8ff242e4c46098b9127ca1fe4e67f82
KING-HANSA <hansadewmina2008@gmail.com>
2025-01-30 21:05:20 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,3 +1,18 @@
+/**
+██████╗ ███████╗██╗    ██╗              ███╗   ███╗██████╗ 
+██╔══██╗██╔════╝██║    ██║              ████╗ ████║██╔══██╗
+██║  ██║█████╗  ██║ █╗ ██║    █████╗    ██╔████╔██║██║  ██║
+██║  ██║██╔══╝  ██║███╗██║    ╚════╝    ██║╚██╔╝██║██║  ██║
+██████╔╝███████╗╚███╔███╔╝              ██║ ╚═╝ ██║██████╔╝
+╚═════╝ ╚══════╝ ╚══╝╚══╝               ╚═╝     ╚═╝╚═════╝ 
+                                                                    
+* Project name - DEW-MD 
+* Author - Hansa Dewmina
+* Team - Dew-coders 
+* Version - vX
+
+World best and powerfull whatsapp user bot in Sri lanka*/
+
 const fs = require('fs');
 if (fs.existsSync('config.env')) require('dotenv').config({ path: './config.env' });
 
@@ -5,6 +20,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "XNR11YIQ#FZuTHzVR8JYnbCp17INiQIVG5VZlNHS5m9t-RB-YnAc",
+SESSION_ID: process.env.SESSION_ID || "qQgmiBAK#QSIAj4Ke6Ztn9-WdX7W1Rlm12_2sP5jR0mB1EObSpWs",
 MONGODB: process.env.MONGODB || "mongodb+srv://Xcelsama:Xcel@xcelsama.qpklf.mongodb.net/?retryWrites=true&w=majority&appName=Xcelsama",
 };
