75f3db2393bd4b3dbcf30cb5cc306277c6c12f1b
KING-HANSA <hansadewmina2008@gmail.com>
2025-01-30 21:34:24 +0530

    Update yts.js

--- plugins/yts.js
+++ plugins/yts.js
@@ -1,18 +1,3 @@
-/**
-██████╗ ███████╗██╗    ██╗              ███╗   ███╗██████╗ 
-██╔══██╗██╔════╝██║    ██║              ████╗ ████║██╔══██╗
-██║  ██║█████╗  ██║ █╗ ██║    █████╗    ██╔████╔██║██║  ██║
-██║  ██║██╔══╝  ██║███╗██║    ╚════╝    ██║╚██╔╝██║██║  ██║
-██████╔╝███████╗╚███╔███╔╝              ██║ ╚═╝ ██║██████╔╝
-╚═════╝ ╚══════╝ ╚══╝╚══╝               ╚═╝     ╚═╝╚═════╝ 
-                                                                    
-* Project name - DEW-MD 
-* Author - Hansa Dewmina
-* Team - Dew-coders 
-* Version - vX
-
-World best and powerfull whatsapp user bot in Sri lanka*/
-
 const config = require('../config')
 const l = console.log
 const { cmd, commands } = require('../command')
