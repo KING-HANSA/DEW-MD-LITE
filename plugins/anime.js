1712b8efa10d04cac70172d5b628b9074072992d
KING-HANSA <hansadewmina2008@gmail.com>
2025-01-30 21:14:53 +0530

    Update anime.js

--- plugins/anime.js
+++ plugins/anime.js
@@ -1,18 +1,3 @@
-/**
-██████╗ ███████╗██╗    ██╗              ███╗   ███╗██████╗ 
-██╔══██╗██╔════╝██║    ██║              ████╗ ████║██╔══██╗
-██║  ██║█████╗  ██║ █╗ ██║    █████╗    ██╔████╔██║██║  ██║
-██║  ██║██╔══╝  ██║███╗██║    ╚════╝    ██║╚██╔╝██║██║  ██║
-██████╔╝███████╗╚███╔███╔╝              ██║ ╚═╝ ██║██████╔╝
-╚═════╝ ╚══════╝ ╚══╝╚══╝               ╚═╝     ╚═╝╚═════╝ 
-                                                                    
-* Project name - DEW-MD 
-* Author - Hansa Dewmina
-* Team - Dew-coders 
-* Version - vX
-
-World best and powerfull whatsapp user bot in Sri lanka*/
-
 const {cmd , commands} = require('../command')
 
 cmd({
