192d2cc7c2a4caf2d533bbf98d837979cac792c9
KING-HANSA <hansadewmina2008@gmail.com>
2025-01-30 19:56:44 +0530

    𝔻𝔼𝕎 - 𝕄𝔻

--- plugins/about.js
+++ plugins/about.js
@@ -1,4 +1,19 @@
-const config = require('../config')
+/**
+██████╗ ███████╗██╗    ██╗              ███╗   ███╗██████╗ 
+██╔══██╗██╔════╝██║    ██║              ████╗ ████║██╔══██╗
+██║  ██║█████╗  ██║ █╗ ██║    █████╗    ██╔████╔██║██║  ██║
+██║  ██║██╔══╝  ██║███╗██║    ╚════╝    ██║╚██╔╝██║██║  ██║
+██████╔╝███████╗╚███╔███╔╝              ██║ ╚═╝ ██║██████╔╝
+╚═════╝ ╚══════╝ ╚══╝╚══╝               ╚═╝     ╚═╝╚═════╝ 
+                                                                    
+* Project name - DEW-MD 
+* Author - Hansa Dewmina
+* Team - Dew-coders 
+* Version - vX
+
+World best and powerfull whatsapp user bot in Sri lanka*/
+
+const {readEnv} = require('../lib/database')
 const {cmd , commands} = require('../command')
 cmd({
     pattern: "about",
@@ -9,9 +24,10 @@ cmd({
 },
 async(conn, mek, m,{from, quoted, body, isCmd, command, args, q, isGroup, sender, senderNumber, botNumber2, botNumber, pushname, isMe, isOwner, groupMetadata, groupName, participants, groupAdmins, isBotAdmins, isAdmins, reply}) => {
 try{
+const config = await readEnv();
 let madeMenu = `❁ ════ ❃•⇆•❃ ════ ❁
 
-*👋 HELLO ${pushname}*\n\n *I AM DEW-MD WHATSAPP BOT\n\n *CREATED BY DEW CODERS*\n\n\n *THANK YOU FOR USING BOT.😊*
+*👋 HELLO ${pushname}*\n\n *I AM DEW-MD WHATSAPP BOT*\n\n *CREATED BY DEW CODERS*\n\n\n *THANK YOU FOR USING BOT.😊*
 
 ❁ ════ ❃•⇆•❃ ════ ❁
 
