68f5e733a3949c8a9f9503c41ac0e11eed814ef3
KING-HANSA <hansadewmina2008@gmail.com>
2025-01-30 19:50:04 +0530

    𝔻𝔼𝕎 - 𝕄𝔻

--- plugins/weather.js
+++ plugins/weather.js
@@ -11,7 +11,7 @@
 * Team - Dew-coders 
 * Version - vX
 
-World best and powerfull whatsapp user bot in Sri lanka*/
+World best and powerfull whatsapp user bot in Sri lanka*/ 
 
 const axios = require('axios');
 const config = require('../config');
